bec85adaaade53bda71e695e63914bd1d8fecf1d
Samour chandra paul <scpsamour@gmail.com>
2026-02-24 00:05:11 +0600

    use Switch statement for increase or decrease
    i have use switch for increase or decrease the input

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,24 +1,30 @@
 import { useState, useReducer, act } from "react";
 
 function reducre(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
-  console.log("this is testing the more and ");
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + 1 };
+    case "dec":
+      return { ...state, count: state.count - 1 };
+    case "setCount":
+      return { ...state, count: action.payload };
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducre, 0);
+  const initialSate = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducre, initialSate);
+  const { count, step } = state;
 
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: "inc"});
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
@@ -35,12 +41,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
