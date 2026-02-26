260f3182d6685cf514e5e269e0b7561c42d41615
Samour chandra paul <scpsamour@gmail.com>
2026-02-26 23:34:31 +0600

    Clear the console
    i have crear all the unnessery console

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function reducer(state, action) {
       return { ...state, status: "active" };
     case "newAnswer":
       const question = state.questions.at(state.index);
-      console.log(question);
       return {
         ...state,
         answer: action.payload,
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,7 +1,7 @@
-function Progress({ index, numQustions,points ,totalPoints}) {
+function Progress({ index, numQustions, points, totalPoints }) {
   return (
     <header className="progress">
-        <progress max={numQustions} value={index}/>
+      <progress max={numQustions} value={index} />
       <p>
         Questions <strong>{index + 1}</strong>/{numQustions}
       </p>
